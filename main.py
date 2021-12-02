2b06ab0c37060c5b2aa5801ca31a094de7374997
Atheer Alattar <aalattar@tibco.com>
2021-12-02 01:23:06 -0600

    Sidebar completed.

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 DISTRIBUTIONS = ['Normal', 'Geometric', 'Uniform', 'Binomial', 'Weibull', 'Gamma']
 st.header('DistSeek: A library to guess the input data distribution')
 
 st.sidebar.title('Choose a starting point')
+
 method = st.sidebar.radio('', ['Input data', 'Generate data'])
 if method == 'Input data':
     st.sidebar.title('Choose data input method')
     input_options = st.sidebar.radio('', ['Upload values (*.csv)', 'Paste values'])
     if input_options == 'Paste values':
-        data = st.sidebar.text_area('Paste your data')
+        raw = st.sidebar.text_area('Paste your data').split('\n')
+        data = pd.DataFrame(raw, columns=['data'])
     else:
-        data = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True)
+        header = st.sidebar.selectbox('Data start at row:', [0, 1])
+        if header == 0:
+            header = None
+        else:
+            header = 0
+            names = ['data']
+        raw = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=False)
+        if raw is not None:
+            data = pd.read_csv(raw, header=header, names=['data'])
+            if data.shape[1] > 1:
+                st.warning(f"Only first column will be used, uploaded data shape is {data.shape}")
+                data = data.iloc[:, 0]
 else:
     selected_dist = st.sidebar.selectbox('Select a distribution (default values provided)', DISTRIBUTIONS)
     n = st.sidebar.text_input('No. of Samples')
@@ -32,3 +46,6 @@ else:
     elif selected_dist == 'Gamma':
         lambda_hat = st.sidebar.text_input('Lambda', value=.161)
         r_hat = st.sidebar.text_input('R', value=.525)
+
+st.write(data)
+st.write(data.shape)
