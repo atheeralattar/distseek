cb67a5407ed443d99764915e522cf837e61a3590
Atheer Alattar <aalattar@tibco.com>
2021-12-02 00:08:54 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+
+DISTRIBUTIONS = ['Normal', 'Geometric', 'Uniform', 'Binomial', 'Weibull', 'Gamma']
+st.header('DistSeek: A library to guess the input data distribution')
+
+st.sidebar.title('Choose a starting point')
+method = st.sidebar.radio('', ['Input data', 'Generate data'])
+if method == 'Input data':
+    st.sidebar.title('Choose data input method')
+    input_options = st.sidebar.radio('', ['Upload values (*.csv)', 'Paste values'])
+    if input_options == 'Paste values':
+        data = st.sidebar.text_area('Paste your data')
+    else:
+        data = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True)
+else:
+    selected_dist = st.sidebar.selectbox('Select a distribution (default values provided)', DISTRIBUTIONS)
+    n = st.sidebar.text_input('No. of Samples')
+    if selected_dist == 'Normal':
+        mu = st.sidebar.text_input('Mean', value=np.random.randint(10, 100))
+        sigma = st.sidebar.text_input('Standard Deviation', value=np.random.randint(1, 10))
+    elif selected_dist == 'Geometric':
+        p = st.sidebar.text_input('P', value=np.round(np.random.rand(), 2))
+    elif selected_dist == 'Uniform':
+        a = st.sidebar.text_input('a', value=np.random.randint(0, 10))
+        b = st.sidebar.text_input('b', value=np.random.randint(11, 20))
+    elif selected_dist == 'Binomial':
+        p = st.sidebar.text_input('P', value=np.round(np.random.rand(), 2))
+    elif selected_dist == 'Weibull':
+        lambda_hat = st.sidebar.text_input('Lambda', value=.161)
+        r_hat = st.sidebar.text_input('R', value=.525)
+    elif selected_dist == 'Gamma':
+        lambda_hat = st.sidebar.text_input('Lambda', value=.161)
+        r_hat = st.sidebar.text_input('R', value=.525)
